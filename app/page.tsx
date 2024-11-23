490dfaf15a68952db63dc52e28e5cff9f9ce66e5
ag-wnl <agnideep@gmail.com>
2024-11-23 15:39:21 +0530

    chore: add on chain identity

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,117 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col gap-[2rem] items-center px-60 py-20 max-md:px-20 max-md::py-10">
+      <section className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">hi, im agwnl</h1>
+        <p>
+          im a developer who builds mostly on the web, interested in
+          decentralisation and algorithmic problems.
+        </p>
+        <p>
+          since end of freshman year in college ive almost always been interning
+          and building passion projects. started out in ml research, moved to a
+          f500 and then to a cool web3 org. learned a lot while working
+          alongside amazing people at my various stints and also got my code
+          serving lots of people!
+        </p>
+      </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <section className="flex flex-col gap-2 w-full">
+        <h1 className="text-2xl font-bold">projects</h1>
+        <p>a lot of my projects are on github</p>
+
+        <ul className="list-disc pl-5">
+          <li>
+            <span className="text-purple-300">readerai</span>: helps you
+            annotate/take notes across the web seamlessly in a new way, answer
+            prompts and everything just a click away!
+          </li>
+          <li>
+            <span className="text-purple-300">clensify</span>: A
+            profanity/bad-word detection library which uses extensive
+            anti-bypass techniques and multi stage detection outperforming
+            conventional detection methods. text pre-processing to decode zalgo,
+            bubble-text, leetspeak etc. and bloom filtering approach for
+            efficient detection. Privated part of this project contains work on
+            contextual embeddings and natural language processing for more
+            accurate prediction of tonality of a text.
+          </li>
+          <li>
+            <span className="text-purple-300">stoira</span>: helping talent
+            collaborate, a social platform with thread like chat scheme where
+            you can discover/find interesting projects to collaborate on.
+          </li>
+          <li>
+            <span className="text-purple-300">slider</span>: find all big-tech
+            and high growth startup openings on a single platform, focused for
+            early career/internships. motivation for this was for personal ease
+            to not juggle between several platforms when finding internships and
+            to also help my friends out :)
+          </li>
+        </ul>
+      </section>
+
+      <section className="flex flex-col gap-2 w-full">
+        <h1 className="text-2xl font-bold">socials</h1>
+        <div className="flex gap-4">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/ag-wnl"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-current hover:text-gray-300"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            <Image width={100} height={100} src="/github.png" alt="github" />
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://linkedin.com/in/agnideep"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-current hover:text-gray-300"
           >
-            Read our docs
+            <Image width={100} height={100} src="/linkedin.png" alt="linkedin" />
+          </a>
+          <a
+            href="https://x.com/ag_wnl"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-current hover:text-gray-300"
+          >
+            <Image width={100} height={100} src="/twitter.png" alt="x.com" />
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section className="flex flex-col gap-2 w-full">
+        <h1 className="text-2xl font-bold">on chain</h1>
+        <div className="flex gap-4">
+          <a
+            href="https://solscan.io/account/FXYJDFZ7TnWBvgnuWUDjCfanxULNNp4aeEddaoHM5Brv"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-current hover:text-gray-300"
+          >
+            <Image width={100} height={100} src="/solana.png" alt="solana" />
+          </a>
+          <a
+            href="https://ethplorer.io/address/0x1894db905f1c473d94ccc80ab4d982416342d549"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-current hover:text-gray-300"
+          >
+            <Image width={100} height={100} src="/ethereum.png" alt="ethereum" />
+          </a>
+          <a
+            href="https://www.mintscan.io/osmosis/address/osmo1w787nw0k7pux6v9al8sqg6y8hk3ycxpl043aa2"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-current hover:text-gray-300"
+          >
+            <Image width={100} height={100} src="/cosmos.png" alt="cosmos" />
+          </a>
+        </div>
+      </section>
     </div>
   );
 }
